45c63c67bed42297e48676920dee5eada080ad1e
Nhefm <minhdao2332@gmail.com>
2023-11-14 20:34:08 +0700

    them delete button o record fragment, chua xoa duoc khi nhan vao item.

--- app/src/main/java/com/duokhongg/audiorecorder/RecordsFragment.java
+++ app/src/main/java/com/duokhongg/audiorecorder/RecordsFragment.java
@@ -13,6 +13,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -28,7 +29,8 @@ public class RecordsFragment extends Fragment implements FragmentCallbacks {
     List<File> listRecordsData;
     MediaPlayer mediaPlayer;
     MainActivity mainActivity;
-
+    Button btnDelete;
+    Boolean is_delete_mode = false;
 
 
     private String getRecordingFilePath(String fileTitle) {
@@ -62,6 +64,7 @@ public class RecordsFragment extends Fragment implements FragmentCallbacks {
         super.onViewCreated(view, savedInstanceState);
 
         listRecords = requireView().findViewById(R.id.listRecords);
+        btnDelete = requireView().findViewById(R.id.btnDelete);
         mediaPlayer = new MediaPlayer();
 
         listRecordsData = getAllRecordsInDirectory();
@@ -73,16 +76,36 @@ public class RecordsFragment extends Fragment implements FragmentCallbacks {
 
 
         listRecords.setOnItemClickListener((parent, view1, position, id) -> {
-            String selectedRecord  = (String) parent.getItemAtPosition(position);
-            Toast.makeText(requireActivity(), selectedRecord + " clicked", Toast.LENGTH_SHORT).show();
-            try {
-                mediaPlayer.setDataSource(getRecordingFilePath(selectedRecord));
-                mediaPlayer.prepare();
-                mediaPlayer.start();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
 
+            String selectedRecord  = (String) parent.getItemAtPosition(position);
+            if (is_delete_mode) {
+               //listRecordsName.remove(position);
+               //listRecords.deferNotifyDataSetChanged();
+               Toast.makeText(requireActivity(), selectedRecord + " is deleted.", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                Toast.makeText(requireActivity(), selectedRecord + " is clicked.", Toast.LENGTH_SHORT).show();
+                try {
+                    mediaPlayer.setDataSource(getRecordingFilePath(selectedRecord));
+                    mediaPlayer.prepare();
+                    mediaPlayer.start();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                is_delete_mode = !is_delete_mode;
+                if(is_delete_mode){
+                    Toast.makeText(requireActivity(), " Delete mode: Enabled", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(requireActivity(), " Delete mode: Disabled", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
     }
 
